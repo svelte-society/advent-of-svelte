253169d60b09faafc1e93c1d7e57dd808ff160e2
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2023-11-30 20:52:27 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -158,7 +158,7 @@
             <h1
                 class="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white"
             >
-                One Svelte challenge for every day of december
+                A new Svelte challenge for every day of december
             </h1>
             <p
                 class="mb-6 max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
@@ -170,7 +170,7 @@
                 href="#"
                 class="inline-flex justify-center items-center py-3 px-5 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
             >
-                Launch SvelteLab
+                See the challenges
                 <svg
                     class="ml-2 -mr-1 w-5 h-5"
                     fill="currentColor"
@@ -184,10 +184,10 @@
                 >
             </a>
             <a
-                href="#"
+                href="https://www.sveltelab.dev/"
                 class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
-                Learn more
+                Launch SvelteLab
             </a>
         </div>
         <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
