64cca140bc21516d65829f9be691ee9f8c67840e
GHOST <ghostdevbusiness@gmail.com>
2023-12-01 13:36:39 +0000

    fix: cors

--- src/routes/data/2023/day-one.json/+server.ts
+++ src/routes/data/2023/day-one.json/+server.ts
@@ -1,26 +1,27 @@
-import { json } from '@sveltejs/kit';
-import { faker } from '@faker-js/faker/locale/en';
+import { json } from '@sveltejs/kit'
+import { faker } from '@faker-js/faker/locale/en'
 
-export const prerender = true;
+export const prerender = true
 
 interface Person {
-	name: string;
-	tally: number;
+	name: string
+	tally: number
 }
 
 export const GET = ({ setHeaders }) => {
 	setHeaders({
 		'cache-control': 'public, max-age=31536000, immutable',
-	});
+		'Access-Control-Allow-Origin': '*',
+	})
 
-	const people: Person[] = [];
+	const people: Person[] = []
 
 	for (let i = 0; i < 100; i++) {
 		people.push({
 			name: faker.person.firstName(),
 			tally: faker.number.int({ min: -100, max: 100 }),
-		});
+		})
 	}
 
-	return json(people);
-};
+	return json(people)
+}
