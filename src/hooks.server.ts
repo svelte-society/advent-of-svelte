0d4bd0919fcfcd78c990469e8d09445cecd63a50
GHOST <ghostdevbusiness@gmail.com>
2023-12-03 23:49:31 +0000

    fix: cors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,11 +3,20 @@ import type { Handle } from '@sveltejs/kit'
 export const handle: Handle = async ({ resolve, event }) => {
 	// Apply CORS header for API routes
 	if (event.url.pathname.startsWith('/data')) {
+		// Required for CORS to work
+		if (event.request.method === 'OPTIONS') {
+			return new Response(null, {
+				headers: {
+					'Access-Control-Allow-Methods':
+						'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Headers': '*',
+				},
+			})
+		}
+
 		event.setHeaders({
-			'Access-Control-Allow-Methods':
-				'GET, POST, PUT, DELETE, PATCH, OPTIONS',
 			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Headers': '*',
 		})
 	}
 
