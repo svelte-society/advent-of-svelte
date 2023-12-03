10fd972ab8190a31b1a7ed7f6ffdb76bb28420d9
GHOST <ghostdevbusiness@gmail.com>
2023-12-03 22:22:09 +0000

    feat: update

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,22 +3,13 @@ import type { Handle } from '@sveltejs/kit'
 export const handle: Handle = async ({ resolve, event }) => {
 	// Apply CORS header for API routes
 	if (event.url.pathname.startsWith('/data')) {
-		// Required for CORS to work
-		if (event.request.method === 'OPTIONS') {
-			return new Response(null, {
-				headers: {
-					'Access-Control-Allow-Methods':
-						'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-					'Access-Control-Allow-Origin': '*',
-					'Access-Control-Allow-Headers': '*',
-				},
-			})
-		}
+		event.setHeaders({
+			'Access-Control-Allow-Methods':
+				'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': '*',
+		})
 	}
 
-	const response = await resolve(event)
-	if (event.url.pathname.startsWith('/data')) {
-		response.headers.append('Access-Control-Allow-Origin', `*`)
-	}
-	return response
+	return await resolve(event)
 }
