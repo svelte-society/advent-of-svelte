ee019546466081c94d17a8c2bfb52f56d439a472
brittneypostma <bdesignedwebdev@gmail.com>
2023-12-02 22:53:02 -0500

    made components for A tag, icons, and pulled out header, footer into layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	// import AnimationFrameSnow from "$lib/components/AnimationFrameSnow.svelte";
 	import { AccordionItem, Accordion } from 'flowbite-svelte'
 	import Countdown from '$lib/components/Countdown.svelte'
 	import santaLogo from '$lib/images/santa-svelte.png'
-	import Snow from '$lib/components/Snow.svelte'
+	import A from '$lib/components/A.svelte'
+	import RightArrow from '$lib/icons/RightArrow.svelte'
+	import SvelteLab from '$lib/icons/SvelteLab.svelte'
 	import { onMount } from 'svelte'
 
 	export let data
@@ -15,37 +16,7 @@
 	})
 </script>
 
-<Snow />
-
-<!-- <AnimationFrameSnow />-->
-<!-- https://github.com/themesberg/tailwind-landing-page -->
-<header>
-	<nav class="border-gray-200 px-4 lg:px-6 py-2 bg-svelte relative">
-		<div
-			class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-			<a href="/" class="flex items-center">
-				<span
-					class="self-center text-xl font-semibold whitespace-nowrap text-white"
-					>Advent of Svelte</span>
-			</a>
-			<div
-				class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-				id="mobile-menu-2">
-				<ul
-					class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-					<li>
-						<a
-							href="/"
-							class="block py-2 pr-4 pl-3 text-white rounded lg:bg-transparent lg:p-0"
-							aria-current="page">2023</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-</header>
-
-<section class="bg-gray-900">
+<section class="bg-gray-900 z-10">
 	<div
 		class="grid py-8 px-4 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 text-center">
 		<div
@@ -55,7 +26,7 @@
 				src={santaLogo}
 				alt="Svelte logo with a santa hat on top of it" />
 		</div>
-		<div class="place-self-center lg:col-span-7">
+		<div class="place-self-center lg:col-span-7 relative">
 			<h1
 				class="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl text-white relative">
 				A Svelte challenge for each day until Christmas!
@@ -67,26 +38,13 @@
 				challenge using Svelte.
 			</p>
 
-			<a
-				href="#challenges"
-				class="inline-flex justify-center items-center py-3 px-5 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-900">
+			<A href="#challenges">
 				See the challenges
-				<svg
-					class="ml-2 -mr-1 w-5 h-5"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-						clip-rule="evenodd" /></svg>
-			</a>
-			<a
-				href="https://www.sveltelab.dev/"
-				target="_blank"
-				class="relative bg-gray-900 inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center rounded-lg border focus:ring-4 text-white border-gray-700 hover:bg-gray-700 focus:ring-gray-800">
+				<RightArrow />
+			</A>
+			<A secondary external href="https://www.sveltelab.dev/">
 				Launch SvelteLab
-			</a>
+			</A>
 		</div>
 		<div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
 			<img
@@ -197,20 +155,16 @@
 								</div>
 
 								<div class="mb-4 flex gap-2">
-									<a
-										class="hover:opacity-80 transition-opacity"
-										href="https://sveltelab.dev/">
-										<img
-											alt="Open in SvelteLab"
-											src="https://docs.sveltelab.dev/button/light_short.svg" />
-									</a>
+									<A external href="https://sveltelab.dev/">
+										Open in SvelteLab
+									</A>
 
-									<a
+									<A
+										external
 										href={challenge.discordLink}
-										target="_blank"
-										class="text-white bg-[#5865F2] hover:opacity-80 transition-opacity focus:ring-4 ring-offset-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
+										class="text-white bg-[#5865F2] hover:bg-indigo-500/60 focus:ring-4 ring-offset-4 font-medium rounded-lg text-sm flex justify-center items-center px-5">
 										Share your solution on Discord
-									</a>
+									</A>
 								</div>
 							</AccordionItem>
 						{/if}
@@ -220,45 +174,6 @@
 		</div>
 	</div>
 </section>
-
-<footer class="p-4 sm:p-6 bg-gray-800 relative">
-	<div class="mx-auto max-w-screen-xl">
-		<div class="md:flex md:justify-between">
-			<div class="mb-6 md:mb-0">
-				<a href="https://sveltesociety.dev/" class="flex items-center">
-					<span
-						class="self-center text-2xl font-semibold whitespace-nowrap text-white"
-						>Advent of Svelte</span>
-				</a>
-			</div>
-		</div>
-
-		<hr class="my-6 sm:mx-auto border-gray-700 lg:my-8" />
-
-		<p class="text-sm text-gray-400">
-			Advent of Svelte is created
-			<a href="https://sveltesociety.dev/">by Svelte Society</a>.
-		</p>
-
-		<p class="text-sm text-gray-400">
-			Santa Hat illustration by
-			<a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">
-				Icons 8
-			</a>
-			from
-			<a href="https://icons8.com/illustrations">Ouch!</a>
-			<br /> Santa sleigh illustration by Illustration by
-			<a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA"
-				>Icons 8</a>
-			from <a href="https://icons8.com/illustrations">Ouch!</a>
-			<br />
-			Cookie Illustration by
-			<a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA"
-				>Icons 8</a>
-			from <a href="https://icons8.com/illustrations">Ouch!</a>
-		</p>
-	</div>
-</footer>
 
 <style>
 	a {
