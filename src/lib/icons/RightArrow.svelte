01cb0fa0fdc31c0c1e1e438c827a5f9cc4a0cf14
GHOST <ghostdevbusiness@gmail.com>
2023-12-04 01:36:11 +0000

    chore: update icons

--- src/lib/icons/RightArrow.svelte
+++ src/lib/icons/RightArrow.svelte
@@ -1,9 +1,25 @@
 <svg
-	class="ml-2 -mr-1 w-5 h-5"
-	fill="currentColor"
-	viewBox="0 0 20 20"
-	xmlns="http://www.w3.org/2000/svg"
-	><path
-		fill-rule="evenodd"
-		d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-		clip-rule="evenodd" /></svg>
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg">
+	<g class="oi-arrow-right">
+		<path
+			class="oi-line"
+			d="M19 12L5 12"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-miterlimit="10"
+			stroke-linecap="round"
+			stroke-linejoin="round" />
+		<path
+			class="oi-incomplete-triangle"
+			d="M13 6L19 12L13 18"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-miterlimit="10"
+			stroke-linecap="round"
+			stroke-linejoin="round" />
+	</g>
+</svg>
