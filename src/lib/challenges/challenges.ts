948791b973ef510c1af669ca8bcee15c9d665bac
GHOST <ghostdevbusiness@gmail.com>
2023-12-04 00:34:38 +0000

    feat: make images work

--- src/lib/challenges/challenges.ts
+++ src/lib/challenges/challenges.ts
@@ -24,32 +24,65 @@ interface ChallengeImport {
 const DEFAULT_DISCORD_LINK =
 	'https://discord.com/channels/457912077277855764/1158027748699279430'
 
+const LOCKED_BODY =
+	'Svelte Bot commends you for trying, but there are no spoilers in Advent of Svelte'
+
 const challengeFiles = import.meta.glob<ChallengeImport>('./day-*.svx', {
 	eager: true,
 })
 
-export const challenges = Object.entries(challengeFiles)
-	// Parse the files to something useful
-	.map<Challenge>(([path, mod]) => {
-		const day = Number(path.slice('./day-'.length, -4))
-		const locked = dev ? false : getDate(NOW_UTC) < day
+export const challenges = await Promise.all(
+	Object.entries(challengeFiles)
+		// Parse the files to something useful
+		.map<Challenge>(([path, mod]) => {
+			const day = Number(path.slice('./day-'.length, -4))
+			const locked = dev ? false : getDate(NOW_UTC) < day
 
-		return {
-			day,
-			locked,
-			body: locked
-				? 'Svelte Bot commends you for trying, but there are no spoilers in Advent of Svelte'
-				: mod.default.render().html,
-			discordLink: mod.metadata?.discordLink || DEFAULT_DISCORD_LINK,
-			image: mod.metadata?.image || null,
-			unlockDate: new Date(
-				`2023-12-${day < 10 ? '0' : ''}${day} 00:00:00 +00:00`,
-			),
-		}
+			return {
+				day,
+				locked,
+				body: locked ? LOCKED_BODY : mod.default.render().html,
+				discordLink: mod.metadata?.discordLink || DEFAULT_DISCORD_LINK,
+				image: mod.metadata?.image || null,
+				unlockDate: new Date(
+					`2023-12-${day < 10 ? '0' : ''}${day} 00:00:00 +00:00`,
+				),
+			}
+		})
+		// Sort it by day
+		.sort((a, b) => a.day - b.day)
+		// Only show the available challenges, and the next day (locked)
+		.filter((challenge) =>
+			dev ? true : isAfter(addDays(NOW_UTC, 1), challenge.unlockDate),
+		)
+		.map(async (challenge) => {
+			const image = challenge.image
+				? // todo lock to webp instead
+				  (await import(`./images/${challenge.image}.png?format=webp`))
+						.default
+				: null
+
+			return {
+				...challenge,
+				image,
+			}
+		}),
+)
+
+// Push a empty locked challenge if the next day hasn't been written yet
+// purely for UI purposes
+// todo handle this in UI instead of complex logic server side
+if (!challenges.some((c) => c.locked)) {
+	const day = challenges.length + 1
+
+	challenges.push({
+		day,
+		locked: true,
+		body: LOCKED_BODY,
+		discordLink: DEFAULT_DISCORD_LINK,
+		image: null,
+		unlockDate: new Date(
+			`2023-12-${day < 10 ? '0' : ''}${day} 00:00:00 +00:00`,
+		),
 	})
-	// Sort it by day
-	.sort((a, b) => a.day - b.day)
-	// Only show the available challenges, and the next day (locked)
-	.filter((challenge) =>
-		dev ? true : isAfter(addDays(NOW_UTC, 1), challenge.unlockDate),
-	)
+}
