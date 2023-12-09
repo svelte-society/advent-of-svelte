8d7d877f1767b1108f75f27c64f0ca049e5a9372
GHOST <ghostdevbusiness@gmail.com>
2023-12-09 23:52:00 +0000

    fix: update days

--- src/routes/data/2023/day-eleven.json/+server.ts
+++ src/routes/data/2023/day-eleven.json/+server.ts
@@ -127,6 +127,12 @@ const names = {
 	],
 }
 
+export const prerender = true
+
 export const GET = ({ setHeaders }) => {
+	setHeaders({
+		'cache-control': 'public, max-age=31536000',
+	})
+
 	return json(names)
 }
