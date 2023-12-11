7f405247cbcd5a2e46df98eb83a311151da96409
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2023-12-12 00:58:51 +0100

    Day 13

---
+++ src/routes/data/2023/day-thirteen.json/+server.ts
@@ -0,0 +1,28 @@
+import { faker } from '@faker-js/faker/locale/en'
+import { json } from '@sveltejs/kit'
+
+export const prerender = true
+
+interface Present {
+	name: string
+	weight: number
+}
+
+export const GET = ({ setHeaders }) => {
+	faker.seed(13)
+
+	setHeaders({
+		'cache-control': 'public, max-age=31536000',
+	})
+
+	const presents: Present[] = []
+
+	for (let i = 0; i < 10000; i++) {
+		presents.push({
+			name: faker.person.firstName(),
+			weight: faker.number.float({ min: 3.5, max: 8.2, precision: 0.01 }),
+		})
+	}
+
+	return json(presents)
+}
