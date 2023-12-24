b87ab5a96e8202f1e7033e5bca936ef6dce29e35
GHOST <ghostdevbusiness@gmail.com>
2023-12-24 00:41:16 +0000

    fix: day 24

--- src/routes/data/2023/day-twenty-four.json/+server.ts
+++ src/routes/data/2023/day-twenty-four.json/+server.ts
@@ -1,13 +1,18 @@
 import { json } from '@sveltejs/kit'
-import { isPast } from 'date-fns'
+import { isFuture, isPast } from 'date-fns'
 import data from './data.json'
 
 export const GET = async () => {
-	const filtered = data.filter((item) => isPast(item.arrival))
-	const now = filtered.at(-1)!
+	const currentIndex = data.findIndex(
+		(dest) =>
+			dest.departure && isPast(dest.arrival) && isFuture(dest.departure),
+	)
 
 	return json({
-		now,
-		destinations: data.filter((item) => isPast(item.arrival)),
+		current: data[currentIndex],
+		next: data[currentIndex + 1],
+		history: data.filter(
+			(item) => item.departure != null && isPast(item.departure),
+		),
 	})
 }
