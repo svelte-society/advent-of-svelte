491240b145b4b758bc4b43a0a9e3326f7183d360
GHOST <ghostdevbusiness@gmail.com>
2024-12-01 05:09:02 +0000

    fix: vercel build

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,17 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import sequence from 'svelte-sequential-preprocessor'
 import { preprocessMeltUI } from '@melt-ui/pp'
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-vercel'
 import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: sequence([mdsvex(), vitePreprocess(), preprocessMeltUI()]),
 	extensions: ['.svelte', '.svx'],
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: 'nodejs22.x',
+		}),
 	},
 }
 export default config
